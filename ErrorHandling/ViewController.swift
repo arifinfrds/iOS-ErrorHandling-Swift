b8989b862ad326c7ceda287c88079c9b7646f200
Arifin Firdaus <arifin.firdaus1996@gmail.com>
2020-09-25 16:22:22 +0700

    Initial Commit

---
+++ ErrorHandling/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ErrorHandling
+//
+//  Created by Arifin Firdaus on 25/09/20.
+//  Copyright © 2020 Arifin Firdaus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
